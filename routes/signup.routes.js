5f435b9609b17203c83b9f94a3e3a66c2324c175
shivaverma1115 <shivaverma1115@gmail.com>
2024-01-29 22:39:52 +0530

    CRUD completed

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -7,7 +7,7 @@ const signupRouter = express.Router();
 const { signupModel } = require('../models/signup.model');
 
 // ===================== CRUD OPERATION =======================
-// ========= Signup Part ================
+// ------------------------ CREATE --------------------------
 signupRouter.post("/", async (req, res) => {
     const { name, age, email, password } = req.body;
     const isUser = await signupModel.findOne({ email: email })
@@ -29,10 +29,60 @@ signupRouter.post("/", async (req, res) => {
             password: hash,
         })
         await newUser.save();
-        return res.send({
+        return res.status(201).send({
             msg: "Sign up successfull"
         })
     })
+})
+
+// ------------------------ READ --------------------------
+
+signupRouter.get('/', async (req, res) => {
+    const { name, email, sort, order,limit } = req.query;
+
+    let query = {};
+    if (name) {
+        query.name = { $regex: name, $options: "i" };
+    }
+    if (email) {
+        query.email = { $regex: email, $options: "i" };
+    }
+
+    let sortOption = {};
+    if (sort) {
+        if (order === "asc" || order === "desc") {
+            sortOption[sort] = order === "asc" ? 1 : -1;
+        }
+    }
+
+    const users = await signupModel.find(query).limit(limit).sort(sortOption);
+    return res.status(200).send(users)
+})
+
+
+// ------------------------ EDIT --------------------------
+
+signupRouter.put('/:_id', async (req, res) => {
+    const user = await signupModel.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true })
+    if (user) {
+        return res.send({
+            msg: "Updated Successfully"
+        })
+    }
+})
+
+// ------------------------ DELETE --------------------------
+
+signupRouter.delete('/:_id', async (req, res) => {
+    const user = await signupModel.findOneAndDelete({ _id: req.params._id })
+    if (user) {
+        return res.send({
+            msg: "Delete Successfully"
+        })
+    }
+    return res.send({
+        msg: "Please try again later"
+    });
 })
 
 
