dd30d403217f650e3e871c9c85891fb6246d8bff
shivaverma1115 <shivaverma1115@gmail.com>
2024-01-30 10:09:41 +0530

    authentication done

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ app.use(express.json()) ;
 const { baseRoute } = require('./routes/baseUrl.route');
 app.use('/', baseRoute);
 
+const {authentication} = require('./middleware/authentication.middleware') ;
+app.use(authentication)
+
 const { signupRouter } = require('./routes/signup.routes');
 app.use('/signup', signupRouter);
 
 const { loginRoute } = require('./routes/login.route');
 app.use('/login', loginRoute);
 
-app.listen(8080, async () => {
+const port = process.env.PORT ;
+app.listen(port, async () => {
     try {
         await connection;
         console.log('your db is connected');
     } catch (error) {
         console.log(error);
     }
-    console.log('your app is running 8080 port');
+    console.log(`your app is running ${port} port`);
 })
